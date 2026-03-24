069d0a3a0d508209b66f78919c6187317298715c
João Pedro Barboza <joaobarboza672@gmail.com>
2026-03-24 09:10:21 -0300

    Add files via upload
    👍👍

--- App.jsx
+++ App.jsx
@@ -1,22 +1,46 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Cadastro from './pages/Cadastro'
-import Catalogo from './pages/Catalogo'
-import Livro from './pages/Livro'
+import {useEffect, useState} from "react";
 
-function App() {
+export default function App() {
+    const [total, setTotal] = useState(0);
+    const [livros, setLivros] = useState([]);
+
+    async function buscarLivros() {
+        let busca = await fetch("https://apps-api-livros.ucxocw.easypanel.host/livro", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+
+        busca = await busca.json()
+
+        setLivros(busca.livros);
+        setTotal(busca.total);
+
+        console.log("Calopsita")
+    }
+
+    useEffect(function () {
+        buscarLivros(
+
+        )
+    }, [])
+
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/cadastro" element={<Cadastro />} />
-                <Route path="/catalogo" element={<Catalogo />} />
-                <Route path="/livro/:id" element={<Livro />} />
-            </Routes>
-        </BrowserRouter>
+        <>
+            <h1>Listagem de Livros</h1>
+            <p>Total de livros {total}</p>
+
+            {livros.map(function (livro, index) {
+                return (
+                    <div key={index}>
+                        <img key={index} src={livro.imagem} />
+                        <h2>{livro.titulo}</h2>
+                        <h3>{livro.autor}</h3>
+                        <p>{livro.categoria}</p>
+                    </div>
+                )
+            })}
+        </>
     )
 }
-
-export default App
